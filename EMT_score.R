1ec5410b3615ada66f25c687fa633113820e5d82
shimw6828 <shimw6828@qq.com>
2017-12-07 14:24:02 +0800

    emt score r

--- EMT_score.R
+++ EMT_score.R
@@ -1,11 +1,11 @@
-expr <- readr::read_rds("/home/shimw/TCGA/pancan33_expr.rds.gz")
+expr <- readr::read_rds("/data/TCGA/TCGA_data/pancan33_expr.rds.gz")
 signature_list <- readr::read_csv("/home/shimw/signature.csv",col_names =T)
 
 filter_makers_gene=function(.x){
     .x %>%
     dplyr::filter(symbol%in%signature_list$X1)
-    
 }
+
 purrr::map(.x=expr$expr,filter_makers_gene) %>%
   tibble::tibble("cancer_types"=expr$cancer_types,"expr"=.) -> filter_gene_expr
 
@@ -37,9 +37,10 @@ EMT_score <- function(x){
   dplyr::select(3:length(names(x))) %>%
   sapply(E_score) -> E  
   
-  tibble::tibble("cell line"=names(M-E),"EMT score"=M-E)
+  tibble::tibble("barcode"=names(M-E),"EMT score"=M-E)
 }
  lapply(filter_gene_expr$expr,EMT_score) %>%
  tibble::tibble("cancer type"=filter_gene_expr$cancer_types,"EMT score"=.)->result
+ readr::write_rds(result,path ="/home/shimw/TCGA/pancan33_EMT_score.rds.gz",compress = "gz")
 
 
