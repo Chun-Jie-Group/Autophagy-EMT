9ddfdc628025203cb2a33e5192d2b6b59d9e1c39
shimw6828 <shimw6828@qq.com>
2017-12-07 11:01:56 +0800

    emt score r

---
+++ EMT_score.R
@@ -0,0 +1,45 @@
+expr <- readr::read_rds("/home/shimw/TCGA/pancan33_expr.rds.gz")
+signature_list <- readr::read_csv("/home/shimw/signature.csv",col_names =T)
+
+filter_makers_gene=function(.x){
+    .x %>%
+    dplyr::filter(symbol%in%signature_list$X1)
+    
+}
+purrr::map(.x=expr$expr,filter_makers_gene) %>%
+  tibble::tibble("cancer_types"=expr$cancer_types,"expr"=.) -> filter_gene_expr
+
+
+
+M_list <- signature_list$X1[signature_list$`E vs M`=="M"]
+E_list <- signature_list$X1[signature_list$`E vs M`=="E"]
+
+M_score <- function(m){
+  m=ifelse(m==0,0.0001,m)
+  sum(log2(m))/55
+}
+
+E_score <- function(m){
+  m=ifelse(m==0,0.0001,m)
+  sum(log2(m))/22
+}
+
+x=filter_gene_expr$expr[[1]]
+
+EMT_score <- function(x){
+  x %>%
+  dplyr::filter(symbol%in%M_list) %>%
+  dplyr::select(3:length(names(x))) %>%
+  sapply(M_score) -> M
+  
+  x %>%
+  dplyr::filter(symbol%in%E_list) %>%
+  dplyr::select(3:length(names(x))) %>%
+  sapply(E_score) -> E  
+  
+  tibble::tibble("cell line"=names(M-E),"EMT score"=M-E)
+}
+ lapply(filter_gene_expr$expr,EMT_score) %>%
+ tibble::tibble("cancer type"=filter_gene_expr$cancer_types,"EMT score"=.)->result
+
+
