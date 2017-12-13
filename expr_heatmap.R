c64d232dc0e3537e7f427e79a67d18597b1ba157
shimw6828 <shimw6828@qq.com>
2017-12-13 09:36:56 +0800

    draw these heatmaps for each tumor type by gene expression

--- expr_heatmap.R
+++ expr_heatmap.R
@@ -17,10 +17,11 @@ purrr::map2(filter_autophage_expr$expr,EMT_sco$EMT_score,function(x,y){
 
 library(ComplexHeatmap)
 library(circlize)
-
+pdf(file.path(pdf_path, "all.pdf"), width=8.5, height = 11)
 purrr::walk2(filter_autophage_cpm$expr,filter_autophage_cpm$cancer_type,function(x,y){
   ha=HeatmapAnnotation(
     df = data.frame(EMT_score=sort(as.numeric(colnames(x[,-1])))),
+  
     col = list(EMT_score=colorRamp2(c(-4, 0, 4), c("blue", "white", "red"), space = "RGB"))
   )
   
@@ -33,20 +34,22 @@ purrr::walk2(filter_autophage_cpm$expr,filter_autophage_cpm$cancer_type,function
       col=colorRamp2(c(-2, 0, 2), c("blue", "white", "yellow"), space = "RGB"),
       name="Expression",
       top_annotation = ha,
+      column_title = y,
+      column_title_side = "bottom",
       show_row_names = T, 
       show_column_names = FALSE, 
       show_row_dend=F,
       show_column_dend=F,
       cluster_columns = FALSE,
       cluster_rows = FALSE,
-      row_names_gp = gpar(fontsize = 5)
-    ) -> ht
-  
-  pdf(file.path(pdf_path, paste(y,".pdf",sep="")), width=8.5, height = 11)
+      row_names_gp = gpar(fontsize = 5),
+      width = 8
+      
+    ) ->ht
   draw(ht)
-  dev.off()
+
 })
 
-
+dev.off()
 
 
