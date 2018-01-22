fb0c1d3aa6c8fde0b988591468cc3f0396a9dd41
shimw6828 <shimw6828@qq.com>
2018-01-22 13:17:55 +0800

    plot the bubble without panel grid

--- Rscript/autophagy_gsva.R
+++ Rscript/autophagy_gsva.R
@@ -2,16 +2,20 @@
 library(magrittr)
 library(GSVA)
 library(ggplot2)
+library(igraph)
+library(ggraph)
 
 # Path --------------------------------------------------------------------
 gene_list_path <- "/project/liucj/projects/6.autophagy/01_autophagy_gene_list/"
 expr_path <- "/data/TCGA/TCGA_data/pancan33_expr.rds.gz"
 EMT_score_path <- "/home/shimw/TCGA/pancan33_EMT_score.rds.gz"
 
+
 # Load data ---------------------------------------------------------------
 expr <- readr::read_rds(expr_path)
 emt_score <-  readr::read_rds(EMT_score_path)
 autophagy_gene <- readr::read_rds(file.path(gene_list_path,"nature_gene_list.rds.gz"))
+
 
 # Calculate gsva for every sample -----------------------------------------
 gene_sets <- list(atg_lys = autophagy_gene$gene_symbol)
@@ -75,11 +79,15 @@ emt_with_gsva%>%
     fdr=p.adjust(Pvalue, method = "fdr")
   ) -> cor_gsva
 
+
+#save the table--------------------------------------------------------------------------
+readr::write_rds(cor_gsva,path ="/home/shimw/TCGA/pancan33_EMT_with_gsva_cor.rds.gz",compress = "gz")
+
 #draw the picture ---------------------------------------------------------
 
 #point----------
 cor_gsva%>%
-  dplyr::filter(abs(correlation)>=0.3)%>%
+  dplyr::filter(abs(correlation)>=0.25)%>%
   dplyr::select(cancer_type,emt_with_gsva)->emt_with_gsva_filter
 
   purrr::map2(emt_with_gsva_filter$cancer_type,emt_with_gsva_filter$emt_with_gsva,function(.x,.y){
@@ -87,8 +95,8 @@ cor_gsva%>%
       tibble::add_column(tumor_type=.x)
     })%>%
     dplyr::bind_rows()%>%
-    ggplot(aes(x=EMT_score,y=gsva)) +geom_point(aes(colour=tumor_type))+
-    scale_color_manual(values=c("#FAD2D9", "#97D1A9", "#A084BD","#6E7BA2","#00A99D"))+
+    ggplot(aes(x=EMT_score,y=gsva)) +geom_point(aes(colour=tumor_type),alpha = .7,size = 3)+
+    scale_color_manual(values=c("#FAD2D9", "#9EDDF9", "#B2509E","#97D1A9","#ED1C24","#F8AFB3","#A084BD","#D97D25","#6E7BA2","#DAF1FC","#00A99D"))+
     theme(
       panel.background = element_rect(colour = "black", fill = "white"),
       panel.grid = element_line(colour = "grey", linetype = "dashed"),
@@ -98,6 +106,11 @@ cor_gsva%>%
         size = 0.2
       ),
       axis.ticks = element_line(color = "black")
+    )+
+    labs(
+      x = "EMT Score",
+      y = "GSVA",
+      colour="Cancer"
     )->p
   ggsave(
     filename = "gsva_point.pdf",
@@ -120,19 +133,22 @@ cor_gsva%>%
 cor_gsva%>%
     dplyr::arrange(desc(correlation))%>%
     dplyr::mutate(fdr = -log10(fdr)) %>% 
-    dplyr::mutate(fdr = ifelse(fdr > 15, 15, fdr))->cor_gsva
-cor_gsva%>%
+    dplyr::mutate(fdr = ifelse(fdr > 15, 15, fdr))%>%
+    dplyr::filter(fdr>-log10(0.05))->cor_gsva_fdr
+  
+cor_gsva_fdr%>%
   ggplot(aes(cancer_type,correlation))+geom_point(aes(size=fdr))+
-  scale_x_discrete(limits=cor_gsva$cancer_type)+
+  scale_x_discrete(limits=cor_gsva_fdr$cancer_type)+
   theme(
     axis.text.x = element_text(angle = 90),
     panel.background = element_rect(colour = "black", fill = "white"),
-    panel.grid = element_line(colour = "grey", linetype = "dashed"),
-    panel.grid.major = element_line(
-      colour = "grey",
-      linetype = "dashed",
-      size = 0.2
-    ),
+   #  panel.grid = element_line(colour = "grey", linetype = "dashed"),
+   # panel.grid.major = element_line(
+   #    colour = "grey",
+   #    linetype = "dashed",
+   #    size = 0.2
+   #  ),
+    panel.grid =element_blank(),
     axis.ticks = element_line(color = "black"),
     legend.text.align = 0.5
   )+
@@ -142,17 +158,45 @@ cor_gsva%>%
     range = c(1, 4),
     breaks = c(-log10(0.05), 5, 10, 15),
     labels = c("0.05", latex2exp::TeX("$10^{-5}$"), latex2exp::TeX("$10^{-10}$"), latex2exp::TeX("$< 10^{-15}$")),
-    
+
     name = "FDR"
+  )+
+  labs(
+    x = "Cancer Type",
+    y = "Correlation"
   )-> p
   ggsave(
-    filename = "gsva_cor_bar.pdf",
+    filename = "gsva_cor_bub.pdf",
     plot = p,
     device = "pdf",
-    width = 8,
-    height = 8,
+    width = 5,
+    height = 4,
     path = "/home/shimw/github/EMT/pdf"
   )
 
 
   
+  cor_gsva_fdr%>%dplyr::select(cancer_type,correlation,fdr)%>%
+    tibble::add_column("EMT"="EMT",.before = 1)%>%
+    graph_from_data_frame(directed = FALSE)->graph_cors
+  ggraph(graph_cors) +
+    geom_edge_link(aes(edge_alpha = abs(correlation), edge_width = fdr, edge_colour = correlation))+
+    guides(edge_alpha = "none")+
+    scale_edge_colour_gradientn(limits = c(-0.3, 0.5), colors = c("firebrick2", "dodgerblue2"))+
+    geom_node_point(color = "#FFEFDB", size = 5)+
+    geom_node_text(aes(label = name), repel = TRUE,size=3) +
+    labs(
+      edge_colour = "Correlation",
+      edge_width = "FDR"
+    )+
+    scale_edge_width_continuous(breaks = c(-log10(0.05), 5, 10, 15),
+                                labels = c("0.05", latex2exp::TeX("$10^{-5}$"), latex2exp::TeX("$10^{-10}$"), latex2exp::TeX("$< 10^{-15}$")))+
+    theme_graph(base_family = "sans")->p
+  ggsave(
+    filename = "gsva_cor_net.pdf",
+    plot = p,
+    device = "pdf",
+    width = 8,
+    height = 7,
+    path = "/home/shimw/github/EMT/pdf"
+  )
